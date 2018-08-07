36ba0c011e738942cb33ad6f94f5e8fed2463bb4
Unknown <dcalva120902@gmail.com>
2018-08-07 09:22:07 +0800

    Twitter Slackbot Process 5
    Scheduling tweets

--- twitterslackbotmiac/process.py
+++ twitterslackbotmiac/process.py
@@ -1,13 +1,12 @@
 from os import environ
-import time
 import re
+from threading import Timer
 import json
 from json import loads
 from tweepy.api import API
 from tweepy.auth import OAuthHandler
 from slackclient import SlackClient
-import sched, time
-
+import schedule, time
 
 # instantiate Slack client
 slack_client = SlackClient(environ.get('SLACK_BOT_TOKEN'))
@@ -19,7 +18,7 @@ RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
 EXAMPLE_COMMAND = "do"
 MENTION_REGEX = "^<@(|[WU].+?)>(.*)"
 
-def trends():
+def trends(channel='assignment1', scheduled=True):
     consumer_key = environ.get('consumer_key', None)
     consumer_secret = environ.get('consumer_secret', None)
     access_token = environ.get('access_token', None)
@@ -43,15 +42,18 @@ def trends():
 
     trending = ', \n'.join(trendy[:10])
 
-    return trending
+    if scheduled:
+        slack_client.api_call(
+            "chat.postMessage",
+            channel=channel,
+            text=trending
+        )
+    else:
+        return trending
 
 
 def parse_bot_commands(slack_events):
-    """
-        Parses a list of events coming from the Slack RTM API to find bot commands.
-        If a bot command is found, this function returns a tuple of command and channel.
-        If its not found, then this function returns None, None.
-    """
+
     for event in slack_events:
         if event["type"] == "message" and not "subtype" in event:
             user_id, message = parse_direct_mention(event["text"])
@@ -60,29 +62,25 @@ def parse_bot_commands(slack_events):
     return None, None
 
 def parse_direct_mention(message_text):
-    """
-        Finds a direct mention (a mention that is at the beginning) in message text
-        and returns the user ID which was mentioned. If there is no direct mention, returns None
-    """
+
     matches = re.search(MENTION_REGEX, message_text)
     # the first group contains the username, the second group contains the remaining message
     return (matches.group(1), matches.group(2).strip()) if matches else (None, None)
 
 def handle_command(command, channel):
-    """
-        Executes bot command if the command is known
-    """
+
     # Default response is help text for the user
     default_response = "Not sure what you mean. Try *{}*.".format(EXAMPLE_COMMAND)
 
     # Finds and executes the given command, filling in response
     response = None
+    scheduled = False
     # This is where you start to implement more commands!
 
     if command.startswith(EXAMPLE_COMMAND):
         response = "Sure...write some more code then I can do that!"
     else:
-        response = trends()
+        response = trends(channel,scheduled)
 
     # Sends the response back to the channel
     slack_client.api_call(
@@ -95,7 +93,10 @@ if __name__ == "__main__":
         print("Starter Bot connected and running!")
         # Read bot's user ID by calling Web API method `auth.test`
         starterbot_id = slack_client.api_call("auth.test")["user_id"]
+        schedule.every().minute.do(trends)
         while True:
+            schedule.run_pending()
+            time.sleep(RTM_READ_DELAY)
             command, channel = parse_bot_commands(slack_client.rtm_read())
             if command:
                 handle_command(command, channel)
